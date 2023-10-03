4e20e4b34d17b5c3244892262bef3a20f428ac12
bismuthadams1 <bismuthadams@gmail.com>
2023-10-03 16:49:19 +0100

    Added xyz_string_to_conf function which converts the xyz file to an openff usable conformer

--- conversion_functions.py
+++ conversion_functions.py
@@ -1,9 +1,11 @@
 from openff.units.elements import SYMBOLS
 from rdkit.Chem import rdmolfiles
 import numpy as np
+from openff.units import unit
 
 
 def conf_to_xyz_string(conformer, molecule):
+    #extract the co-ordinates from the conformer array and the atoms from the openff molecule object. 
     atoms = [
                     {
                         "element": SYMBOLS[atom.atomic_number],
@@ -13,10 +15,22 @@ def conf_to_xyz_string(conformer, molecule):
                     }
                     for index, atom in enumerate(molecule.atoms)
                 ]
+    #format the top of the xyz file with the number of atoms and the smiles string
     xyz = f'{molecule.n_atoms}\n{molecule.to_smiles()}\n'
     for row in atoms:
-        xyz += f"{row['element']}\t{np.around(row['x'].magnitude,decimals=6)}\t{np.around(row['y'].magnitude,decimals=6)}\t{np.around(row['z'].magnitude, decimals=6)}\n"
+        xyz += f"{row['element']}\t{np.around(row['x'].magnitude,decimals=8)}\t{np.around(row['y'].magnitude,decimals=8)}\t{np.around(row['z'].magnitude, decimals=8)}\n"
     return xyz
 
-#def xyz_string_to_conf(xyz_string, conformer):
-    
+def xyz_string_to_conf(xyz_string, molecule):
+
+    #skip the heading of the xyz_string 
+    xyz_list = np.array([row.split('\t') for row in xyz_string.split('\n')[2:]][:-1])
+
+    conformer_from_xyz = np.zeros((molecule.n_atoms, 3))
+
+    for atom_index, coordinates in enumerate(xyz_list):
+        #append to conformer array, skip the element in the xyz file
+        conformer_from_xyz[atom_index, :] = coordinates[1:] 
+
+    return conformer_from_xyz
+
