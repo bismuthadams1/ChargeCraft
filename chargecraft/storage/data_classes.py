ddfe6eb3449d0f77659dae37619e0dcc686b3320
bismuthadams1 <bismuthadams@gmail.com>
2024-01-23 15:36:53 +0000

    fixed pydantic solvent epsilon issue

--- chargecraft/storage/data_classes.py
+++ chargecraft/storage/data_classes.py
@@ -23,7 +23,7 @@ class DDXSettings(BaseModel):
     description="The solvent to simulate. This can be 'Water' as a string",
     )
 
-    epsilon: Optional[str] = Field(None,
+    epsilon: Optional[float] = Field(None,
     description="the dialectric constant of the fluid")
 
     radii_set: Literal["uff","bondi"] = Field("uff",
