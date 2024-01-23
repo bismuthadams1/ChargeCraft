2eb410c03ccaab1be237e37d664c27fbefef700d
bismuthadams1 <bismuthadams@gmail.com>
2024-01-23 15:36:53 +0000

    fixed pydantic solvent epsilon issue

--- examples/ddx_example.py
+++ examples/ddx_example.py
@@ -10,12 +10,16 @@ H 1 0.96 2 104.5
 psi4.set_options({"ddx": "true", #supply a solvent here to see if epsilon then gets picked up
                 "DDX_SOLVENT_EPSILON":5,
                 "DDX_RADII_SET": 'uff',
+<<<<<<< HEAD
                 "DDX_MODEL": 'PCM',
                 "DDX_SOLVENT":""})  #without this calculation fails with psi4.driver.p4util.exceptions.ValidationError: Required option 'DDX_SOLVENT' is missing.
+=======
+                "DDX_MODEL": 'PCM'})  #without this calculation fails with psi4.driver.p4util.exceptions.ValidationError: Required option 'DDX_SOLVENT' is missing.
+>>>>>>> ddfe6eb (fixed pydantic solvent epsilon issue)
 
 psi4.set_options({"PROPERTIES_ORIGIN":[0,0,0]})
 core.print_global_options()
 psi4.set_memory('500mb')
-E, wfn = psi4.prop('scf/sto-3g', properties=["DIPOLE"], return_wfn = True)
+E, wfn = psi4.prop('HF/sto-3g', properties=["DIPOLE"], return_wfn = True)
 # psi4.energy('scf/sto-3g')
-print(wfn.variable('DIPOLE'))
+print(wfn.variable('HF DIPOLE'))
